1077b2648c99d63c08df9a3453825f970d3805fd
Ninhydro <ninosanjaya1998@gmail.com>
2026-01-23 21:32:10 +0100

    23/1/2026 v1

--- dialogic/cutscene/cutscene9.gd
+++ dialogic/cutscene/cutscene9.gd
@@ -139,17 +139,19 @@ func _setup_cutscene():
 	
 	if Global.first_boss_dead:#first boss dead
 		sequence = [
+		{"type": "move_player", "name": "marker1",  "duration": 0.1, "animation": "run", "wait": false},
+		{"type": "player_face", "direction": -1}, #1 is right, -1 is left
 		{"type": "wait", "duration": 0.5},
 		{"type": "fade_out", "wait": false},
 		
-		{"type": "player_face", "direction": -1}, #1 is right, -1 is left
-		{"type": "move_player", "name": "marker1",  "duration": 2, "animation": "run", "wait": false},
+		#{"type": "player_face", "direction": -1}, #1 is right, -1 is left
+		#{"type": "move_player", "name": "marker1",  "duration": 2, "animation": "run", "wait": false},
 		{"type": "animation", "name": "anim1v2", "wait": true, "loop": false},
 		{"type": "player_animation", "name": "idle",  "wait": false},
 		{"type": "animation", "name": "anim1v2_idle", "wait": false, "loop": true},
 		{"type": "dialog", "name": "timeline10v2", "wait": true},
 		{"type": "animation", "name": "anim2v2", "wait": true, "loop": false},
-		{"type": "animation", "name": "anim2_idle", "wait": false, "loop": true},
+		{"type": "animation", "name": "anim2v2_idle", "wait": false, "loop": true},
 		{"type": "dialog", "name": "timeline10_5", "wait": true},
 		
 		{"type": "wait", "duration": 0.1},		
@@ -161,11 +163,11 @@ func _setup_cutscene():
 		#Dialogic.start("timeline10v2", false)
 	else: #first boss not dead
 		sequence = [
+		{"type": "move_player", "name": "marker1",  "duration": 0.1, "animation": "run", "wait": false},
+		{"type": "player_face", "direction": -1}, #1 is right, -1 is left
 		{"type": "wait", "duration": 0.5},
 		{"type": "fade_out", "wait": false},
 		
-		{"type": "player_face", "direction": -1}, #1 is right, -1 is left
-		{"type": "move_player", "name": "marker1",  "duration": 2, "animation": "run", "wait": false},
 		{"type": "animation", "name": "anim1", "wait": true, "loop": false},
 		{"type": "player_animation", "name": "idle",  "wait": false},
 		{"type": "animation", "name": "anim1_idle", "wait": false, "loop": true},
